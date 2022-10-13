085c38f7bd638f715da5e4124c7a923aff59d874
User <kyle_albright@hotmail.com>
2022-10-13 17:50:17 -0500

    added if else statement to make timeblocks correct color

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,29 @@
 var currentDay = $("#currentDay");
-currentDay.text (moment().format("dddd, MMM Do, YYYY"))
+currentDay.text(moment().format("dddd, MMM Do, YYYY"));
+
+var currentTime = moment().hour();
+console.log(currentTime);
+
+$(".time-block").each(function () {
+  var scheduleTime = $(this).attr("id");
+  console.log(scheduleTime);
+
+  if (scheduleTime < currentTime) {
+    $(this).children("textarea").addClass("past");
+  } else if (scheduleTime == currentTime) {
+    $(this).children("textarea").addClass("present");
+  } else {
+    $(this).children("textarea").addClass("future");
+  }
+});
+
+$(".saveBtn").on("click", function () {
+
+   
+
+  var userInput = $(this).parent().find(".description").val();
+
+  localStorage.setItem("savedInfo", userInput);
+
+  localStorage.getItem("savedInfo")
+});
